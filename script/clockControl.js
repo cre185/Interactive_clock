ba4fea24094f14a4e9d3d25638aa06433414accf
hellex <1443006948@qq.com>
2023-07-19 16:46:10 +0800

    test

---
+++ script/clockControl.js
@@ -0,0 +1,33 @@
+var embedSVG = document.getElementById("clock_svg");
+    var namespace = "http://www.w3.org/2000/svg";
+    // 绘制表盘
+    for(i = 0; i < 60; i++){
+        var rect = document.createElementNS(namespace, "rect");
+        var width;
+        var height
+        var r;
+        rect.setAttribute("fill", "black")
+        if(i % 15 == 0){
+            continue;
+        }
+        else if(i % 5 === 0){
+            width = 12;
+            height = 30;
+            r = 120;
+        }
+        else{
+            width = 6;
+            height = 12;
+            r = 128
+        }
+        let x = (150 + r * Math.sin(Math.PI * i / 30) - width / 2).toString();
+        let y = (150 - r * Math.cos(Math.PI * i / 30) - height / 2).toString();
+        rect.setAttribute("width", width);
+        rect.setAttribute("height", height);
+        rect.setAttribute("x", x);
+        rect.setAttribute("y", y);
+        x = (150 + r * Math.sin(Math.PI * i / 30)).toString();
+        y = (150 - r * Math.cos(Math.PI * i / 30)).toString();
+        rect.setAttribute("transform", "rotate(" + (i * 6).toString() +", " + x + ", " + y + ")");
+        embedSVG.appendChild(rect);
+    }
--- script/navigatorControl.js
+++ script/navigatorControl.js
@@ -2,6 +2,10 @@
 var navigatorControl={};
 var fullpath = location.href.split('/');
 navigatorControl.htmlName = fullpath[fullpath.length - 1];
+var clockPage=function(){
+    if(navigatorControl.htmlName === "clock.html"){ return; }
+    window.location.replace("clock.html");
+}
 var alarmClockPage=function(){
     if(navigatorControl.htmlName==="alarmClock.html"){ return; }
     window.location.replace("alarmClock.html");
@@ -18,15 +22,18 @@ var timerPage=function(){
 // insertion of navigator
 document.writeln("<div id=\'navigator\'>");
 document.writeln("    <ul>");
+document.writeln("        <li id=\'clock\'>时钟</li>");
 document.writeln("        <li id=\'alarmClock\'>闹钟</li>");
 document.writeln("        <li id=\'stopWatch\'>秒表</li>");
 document.writeln("        <li id=\'timer\'>计时器</li>");
 document.writeln("    </ul>");
 document.writeln("</div>");
 
+navigatorControl.clock=$('#clock').get(0);
 navigatorControl.alarmClock=$('#alarmClock').get(0);
 navigatorControl.stopWatch=$('#stopWatch').get(0);
 navigatorControl.timer=$('#timer').get(0);
+navigatorControl.clock.onclick = clockPage;
 navigatorControl.alarmClock.onclick=alarmClockPage;
 navigatorControl.stopWatch.onclick=stopWatchPage;
 navigatorControl.timer.onclick=timerPage;
