c30b4829635eecd89ad5d3f6f03168a01821f08c
YaoRunmao <1969623002@qq.com>
2023-07-21 19:28:33 +0800

    秒表新增指针显示

--- script/stopWatchControl.js
+++ script/stopWatchControl.js
@@ -54,11 +54,6 @@ class stopwatch extends time
         this.mSec = 0;
         this.clearTimer();
     }
-
-    set_recordTimer(timer)
-    {
-        this.recordtimer = timer;
-    }
 }
 
 // 秒表初始化
@@ -68,6 +63,7 @@ var stopWatchControl={};
 stopWatchControl.on = false;
 // 分段
 stopWatchControl.recordCount = 0;
+stopWatchControl.current = global.stopWatchTime;
 
 function appendRecord(t){
     // 右边栏添加显示组件
@@ -101,6 +97,7 @@ function startStopwatch(){
     let timer = setInterval(function (){
         global.stopWatchTime.tick();
         document.getElementById('stopwatch').innerText = global.stopWatchTime.toString();
+        initClock();
     },10);
     global.stopWatchTime.setTimer(timer);
     // 秒表启动后，自动有第一个分段
@@ -142,7 +139,10 @@ function restartStopwatch(){
     document.getElementById('start').innerText = "启动";
     // 秒表复位
     global.stopWatchTime.clear();
+    stopWatchControl.current.clear();
+    clearInterval(global.stopWatchTime.recordtimer);
     document.getElementById('stopwatch').innerText = global.stopWatchTime.toString();
+    initClock();
 }
 function recordStopwatch(){
     // 只有在秒表运行的时候才能分段
@@ -160,6 +160,16 @@ function recordStopwatch(){
 // 显示部分由数码切换至指针
 function number_to_pointer()
 {
+    if(stopWatchControl.on)
+    {
+        global.stopWatchTime.clearTimer();
+        let timer = setInterval(function (){
+            global.stopWatchTime.tick();
+            document.getElementById('stopwatch').innerText = global.stopWatchTime.toString();
+            initClock();
+        },10);
+        global.stopWatchTime.setTimer(timer);
+    }
     document.getElementById('stopwatch').style.visibility = "hidden";
     document.getElementById('stopwatch_pointer').style.visibility = "visible";
 }
@@ -167,11 +177,29 @@ function number_to_pointer()
 // 显示部分由指针切换至数码
 function pointer_to_number()
 {
+    if(stopWatchControl.on)
+    {
+        global.stopWatchTime.clearTimer();
+        let timer = setInterval(function (){
+            global.stopWatchTime.tick();
+            document.getElementById('stopwatch').innerText = global.stopWatchTime.toString();
+            initClock();
+        },10);
+        global.stopWatchTime.setTimer(timer);
+    }
     document.getElementById('stopwatch_pointer').style.visibility = "hidden";
     document.getElementById('stopwatch').style.visibility = "visible";
 }
 
-function drawClock(){
+// 初始化表+更新表
+function initClock()
+{
+    drawBigClock();
+    drawSmallClock();
+}
+
+// 画大表
+function drawBigClock(){
     const clock= document.getElementById("stopwatch_pointer");
     cx = clock.scrollWidth * 0.5;
     cy = clock.scrollWidth * 0.5;
@@ -184,23 +212,118 @@ function drawClock(){
     let unit = cx / 300;
     // 表盘半径
     let radius = cx * 0.6;
-    // 绘制表盘
+    // 绘制大表盘
     for(let i = 0; i < 240; i++){
         let rect = document.createElementNS(namespace, "rect");
         // 刻度尺寸参数
         let width;
         let height;
-        // 刻度半径
-        let r = radius - height / 2;
-        rect.setAttribute("fill", "black");
         if(i % 20 === 0){
             width = 3 * unit;
             height = 15 * unit;
+            rect.setAttribute("fill", "black");
         }
         else{
             width = 3 * unit;
             height = 8 * unit;
+            rect.setAttribute("fill", "gray");
         }
+        // 刻度半径
+        let r = radius - height / 2;
+        // 刻度中心坐标
+        let x = (cx + r * Math.sin(Math.PI * i / 120) - width / 2).toString();
+        let y = (cy - r * Math.cos(Math.PI * i / 120) - height / 2).toString();
+        // 设置刻度属性
+        rect.setAttribute("width", width);
+        rect.setAttribute("height", height);
+        rect.setAttribute("x", x);
+        rect.setAttribute("y", y);
+        x = (cx + r * Math.sin(Math.PI * i / 120)).toString();
+        y = (cy - r * Math.cos(Math.PI * i / 120)).toString();
+        rect.setAttribute("transform", "rotate(" + (i * 1.5).toString() +", " + x + ", " + y + ")");
+        scale.appendChild(rect);
+    }
+    // 绘制表针
+    {
+        function setAttributes(width, height, color, angle){
+            this.setAttribute("width", width.toString());
+            this.setAttribute("height", height.toString());
+            this.setAttribute("x", (cx - width / 2).toString());
+            this.setAttribute("y", (cy - height).toString());
+            this.setAttribute("transform", "rotate(" + angle.toString() +", " + cx + ", " + cy + ")");
+            this.setAttribute("fill", color);
+        }
+
+        Object.defineProperty(SVGElement.prototype, "set", {
+            value: setAttributes,
+            writable: true,
+            enumerable: false,
+            configurable: true,
+        });
+
+        // 分段长指针
+        let hand_record_long = document.createElementNS(namespace, "rect");
+        hand_record_long.setAttribute("id", "hand_record_long");
+        hand_record_long.set(3*unit, radius, "blue", stopWatchControl.current.calSecAngle());
+        hands.appendChild(hand_record_long);
+        // 分段短指针
+        let hand_record_short = document.createElementNS(namespace, "rect");
+        hand_record_short.setAttribute("id", "hand_record_short");
+        hand_record_short.set(3*unit, radius*0.2, "blue", stopWatchControl.current.calSecAngle()+180);
+        hands.appendChild(hand_record_short);
+        // 长指针
+        let hand_long = document.createElementNS(namespace, "rect");
+        hand_long.setAttribute("id", "hand_long");
+        hand_long.set(3*unit, radius, "orange", global.stopWatchTime.calSecAngle());
+        hands.appendChild(hand_long);
+        // 短指针
+        let hand_short = document.createElementNS(namespace, "rect");
+        hand_short.setAttribute("id", "hand_short");
+        hand_short.set(3*unit, radius*0.2, "orange", global.stopWatchTime.calSecAngle()+180);
+        hands.appendChild(hand_short);
+    }
+}
+
+// 画小表
+function drawSmallClock()
+{
+    const clock= document.getElementById("stopwatch_pointer");
+    cx = clock.scrollWidth * 0.5;
+    cy = clock.scrollWidth * 0.35;
+    const scale= document.getElementById("scale_min");
+    const hands= document.getElementById("hands_min");
+    scale.innerHTML = "";
+    hands.innerHTML = "";
+    let namespace = "http://www.w3.org/2000/svg";
+    // 单位长度
+    let unit = cx / 300;
+    // 表盘半径
+    let radius = cx * 0.15;
+    // 绘制大表盘
+    for(let i = 0; i < 60; i++){
+        let rect = document.createElementNS(namespace, "rect");
+        // 刻度尺寸参数
+        let width;
+        let height;
+        if(i % 2 === 0){
+            width = 2 * unit;
+            height = 10 * unit;
+            if(i % 10 === 0)
+            {
+                rect.setAttribute("fill", "black");
+            }
+            else
+            {
+                rect.setAttribute("fill", "gray");
+            }
+        }
+        else{
+            width = 2 * unit;
+            height = 5 * unit;
+            rect.setAttribute("fill", "gray");
+        }
+        // 刻度半径
+        let r = radius - height / 2;
         // 刻度中心坐标
         let x = (cx + r * Math.sin(Math.PI * i / 30) - width / 2).toString();
         let y = (cy - r * Math.cos(Math.PI * i / 30) - height / 2).toString();
@@ -232,29 +355,11 @@ function drawClock(){
             configurable: true,
         });
 
-        // 时针
-        var hrHand = document.createElementNS(namespace, "rect");
-        hrHand.setAttribute("id", "hourHand");
-        width = 15 * v;
-        height = radius * 0.5;
-        hrHand.set(width, height, "gray", global.globalTime.calHourAngle());
-        hands.appendChild(hrHand);
-
-        // 分针
-        var minHand = document.createElementNS(namespace, "rect");
-        minHand.setAttribute("id", "minuteHand");
-        width = 8 * v;
-        height = radius * 0.6;
-        minHand.set(width, height, "black", global.globalTime.calMinAngle());
-        hands.appendChild(minHand);
-
-        // 秒针
-        var secHand = document.createElementNS(namespace, "rect");
-        secHand.setAttribute("id", "secondHand");
-        width = 2 * v;
-        height = radius * 0.9;
-        secHand.set(width, height, "red", global.globalTime.calSecAngle());
-        hands.appendChild(secHand);
+        // 指针
+        let hand_long = document.createElementNS(namespace, "rect");
+        hand_long.setAttribute("id", "hand_long");
+        hand_long.set(2*unit, radius, "orange", 2*global.stopWatchTime.calMinAngle());
+        hands.appendChild(hand_long);
     }
 }
 
